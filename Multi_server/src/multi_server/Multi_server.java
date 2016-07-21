9d0a78952caf481221b98175031dfc3a3bbe9319
01917106610rR110221 <rubelcseku11@gmail.com>
2016-07-21 23:52:44 +0600

    First commit

---
+++ Multi_server/src/multi_server/Multi_server.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package multi_server;
+
+/**
+ *
+ * @author Rubel
+ */
+public class Multi_server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        multiServer obj = new multiServer();
+        obj.show();
+    }
+    
+}
